0eb6f8eb1723e79a000ce085a142fe28a362b49d
ElenaUJ <elena-ha@web.de>
2023-01-02 08:41:18 -0500

    Added express and basic GET routes to app

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express'),
+  morgan = require('morgan'),
+
+// Variable for Express' functionality to configure web server
+const app = express();
+
+let topMovies = [
+  {
+    title: 'Forrest Gump',
+    director: 'Robert Zemeckis',
+    release: '1994',
+  },
+  {
+    title: 'The Truman Show',
+    director: 'Peter Weir',
+    release: '1998',
+  },
+  {
+    title: 'Before Sunrise',
+    director: 'Richard Linklater',
+    release: '1995',
+  },
+  {
+    title: 'Sonic the Hedgehog 2',
+    director: 'Jeff Fowler',
+    release: '2022',
+  },
+  {
+    title: 'Avatar',
+    director: 'James Cameron',
+    release: '2009',
+  },
+  {
+    title: 'Pulp Fiction',
+    director: 'Quentin Tarantino',
+    release: '1994',
+  },
+  {
+    title: 'Gone with the Wind',
+    director: 'Victor Fleming',
+    release: '1939',
+  },
+  {
+    title: 'Madagascar',
+    director: 'Eric Darnell, Tom McGrath',
+    release: '2005',
+  },
+  {
+    title: 'The Green Mile',
+    director: 'Frank Darabont',
+    release: '1999',
+  },
+  {
+    title: 'Dead Poets Society',
+    director: 'Peter Weir',
+    release: '1989',
+  },
+];
+
+// Logging requests to console
+app.use(morgan('common'));
+
+// Question: What does this actually do? Wouldn't the request to send the documentation file work regardless?
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('Movie database is being contructed.');
+});
+
+app.get('/movies', function (req, res) {
+  res.json(topMovies);
+});
+
+app.get('/documentation', function (req, res) {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.listen(8080, function () {
+  console.log('MyFlix app is listening to port 8080.');
+});
