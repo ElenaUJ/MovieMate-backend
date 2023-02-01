9fbe48460210c3dd11885629860c4bf7f78fc053
ElenaUJ <elena-ha@web.de>
2023-02-01 13:05:29 -0500

    Cleaned up index.js code

--- index.js
+++ index.js
@@ -1,74 +1,51 @@
-const express = require('express'),
-  morgan = require('morgan'),
-  bodyParser = require('body-parser'),
-  fs = require('fs'),
-  path = require('path'),
-  mongoose = require('mongoose'),
-  Models = require('./models.js');
-
-// Variable for Express' functionality to configure web server
+const express = require('express');
 const app = express();
 
+// Parsing incoming requests
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
-// Parses bodies from URL requests, extended: true means req.body object can contain any value, not just strings
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Cors middleware. Making sure that app uses CORS specifying domains which get access to API
+// CORS middleware
 const cors = require('cors');
-// Per default, this grants access to all origins
+
 app.use(cors());
 
-// The fewer domains have access, the more secure - if I only wanted to give certain domains access, I would do it like this:
-// let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       // Question: Why is that here? If there is no origin header- why should that grant access?
-//       if (!origin) {
-//         return callback(null, true);
-//       }
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         let message =
-//           "The CORS policy for this application doesn't allow access from origin" +
-//           origin;
-//         // false means access to API will be denied
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
-
-// Import of auth.js file, must come after bodyparser middleware! App argument ensures that Express is available in auth.js file, too
-// Question: How is it enough to import the file? How does the app understand that when the /login endpoint is requested, it has to look in the auth.js file for the logic?
-//Passport module and passport.js file are required, too
+// Authentication (auth.js is handling login endpoint and generating JWT tokens)
 let auth = require('./auth.js')(app);
 const passport = require('passport');
+
 require('./passport.js');
 
 // Input validation
 const { check, validationResult } = require('express-validator');
 
-// Refer to model names defined in models.js file
+// Integrating Mongoose and connecting to MongoDB
+const mongoose = require('mongoose');
+const Models = require('./models.js');
 const Movies = Models.Movie;
 const Users = Models.User;
 
 // Suppresses deprecation warning
 mongoose.set('strictQuery', true);
-// Allows Mongoose to connect to that database so it can perform CRUD operations on its documents from within REST API
 mongoose.connect('mongodb://localhost:27017/myFlixDB');
 
-//Routing of static files
+//Routing of static files (e.g. documentation.html)
 app.use(express.static('public'));
 
-// Logging requests to console
-app.use(morgan('common'));
-// Additionally, creating log stream in log.txt file (flags: 'a' instructs to append logs to file)
+// Logging requests
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
   flags: 'a',
 });
+
+app.use(morgan('common'));
 app.use(morgan('common', { stream: accessLogStream }));
 
+//------------------ Endpoint handlers ------------------//
 app.get('/', function (req, res) {
   res.send('Movie database is being contructed.');
 });
@@ -94,13 +71,14 @@ app.get(
   '/movies/:title',
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const title = req.params.title;
-    Movies.findOne({ Title: title })
+    Movies.findOne({ Title: req.params.title })
       .then(function (movie) {
         if (!movie) {
           res
             .status(404)
-            .send('Movie with the title of ' + title + ' was not found.');
+            .send(
+              'Movie with the title of ' + req.params.title + ' was not found.'
+            );
         } else {
           res.status(200).json(movie);
         }
@@ -115,16 +93,15 @@ app.get(
 // Gets data about a genre by genre name
 app.get(
   '/movies/genres/:genreName',
-  // Question: How does the app know which strategy to use? What if there were more jwt strategies in the passport.js file?
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const genreName = req.params.genreName;
-    Movies.findOne({ 'Genre.Name': genreName })
+    Movies.findOne({ 'Genre.Name': req.params.genreName })
       .then(function (genre) {
         if (!genre) {
-          return res.status(404).send('Genre ' + genreName + ' was not found.');
+          return res
+            .status(404)
+            .send('Genre ' + req.params.genreName + ' was not found.');
         } else {
-          // .Genre has to be added so only the genre key is returned
           res.status(200).json(genre.Genre);
         }
       })
@@ -140,12 +117,13 @@ app.get(
   '/movies/genres/:genreName/movies',
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const genreName = req.params.genreName;
-    Movies.find({ 'Genre.Name': genreName })
+    Movies.find({ 'Genre.Name': req.params.genreName })
       .select('Title')
       .then(function (movieTitles) {
         if (movieTitles.length === 0) {
-          return res.status(404).send('Genre ' + genreName + ' was not found.');
+          return res
+            .status(404)
+            .send('Genre ' + req.params.genreName + ' was not found.');
         } else {
           res.status(200).json(movieTitles);
         }
@@ -162,14 +140,15 @@ app.get(
   '/movies/directors/:directorName',
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const directorName = req.params.directorName;
-    Movies.findOne({ 'Director.Name': directorName })
+    Movies.findOne({ 'Director.Name': req.params.directorName })
       .then(function (director) {
         if (!director) {
           res
             .status(404)
             .send(
-              'Director with the name of ' + directorName + ' was not found.'
+              'Director with the name of ' +
+                req.params.directorName +
+                ' was not found.'
             );
         } else {
           res.status(200).json(director.Director);
@@ -187,15 +166,16 @@ app.get(
   '/movies/directors/:directorName/movies',
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const directorName = req.params.directorName;
-    Movies.find({ 'Director.Name': directorName })
+    Movies.find({ 'Director.Name': req.params.directorName })
       .select('Title')
       .then(function (movieTitles) {
         if (movieTitles.length === 0) {
           return res
             .status(404)
             .send(
-              'Director with the name of ' + directorName + ' was not found.'
+              'Director with the name of ' +
+                req.params.directorName +
+                ' was not found.'
             );
         } else {
           res.status(200).json(movieTitles);
@@ -208,10 +188,9 @@ app.get(
   }
 );
 
-// Registers new user (and hashes their password)
+// Registers new user
 app.post(
   '/users',
-  // Validation logic
   [
     check('Username')
       .not()
@@ -233,26 +212,22 @@ app.post(
       .withMessage('Email does not appear to be valid.'),
   ],
   function (req, res) {
-    // validationResult is a method provided by express-validator library
     let errors = validationResult(req);
+
     if (!errors.isEmpty()) {
-      // array() is another method of the express-validator library
-      // Status code 422: unprocessable entity
-      // Rest of code will not be executed: database remains safe from potential malicious code
       return res.status(422).json({ errors: errors.array() });
     }
 
     let hashedPassword = Users.hashPassword(req.body.Password);
 
-    const username = req.body.Username;
-    Users.findOne({ Username: username })
+    Users.findOne({ Username: req.body.Username })
       .then(function (user) {
         if (user) {
-          return res.status(409).send(username + ' already exists.');
+          return res.status(409).send(req.body.Username + ' already exists.');
         } else {
           // .create command takes object. Keys specified in Schema, values received by request body.
           Users.create({
-            Username: username,
+            Username: req.body.Username,
             Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday,
@@ -273,38 +248,13 @@ app.post(
   }
 );
 
-// Gets data about a user by name
-// Will be deleted before app goes live
-app.get('/users/:username', function (req, res) {
-  const username = req.params.username;
-  Users.findOne({ Username: username })
-    .then(function (user) {
-      if (!user) {
-        res.status(404).send('No user with the name of ' + username);
-      } else {
-        res.status(200).json(user);
-      }
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.status(500).send('Error: ' + err);
-    });
-});
-
-// get all users //delete!!!
-app.get('/users', function (req, res) {
-  Users.find().then(function (users) {
-    res.json(users);
-  });
-});
-
 // Updates user info by username
 app.put(
   '/users/:username',
   passport.authenticate('jwt', { session: false }),
   [
     check('Username')
-      // added optional() method, because not necessarily all fields will be updated
+      // Added optional() method because not necessarily all fields will be updated
       .optional()
       .isAlphanumeric()
       .withMessage(
@@ -317,18 +267,18 @@ app.put(
   ],
   function (req, res) {
     let errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
 
     const currentUsername = req.params.username;
     const newUsername = req.body.Username;
-    const newPassword = req.body.Password;
 
     function updateUser() {
       // If statement necessary to prevent bcrypt error when no passwort is request body
-      if (newPassword) {
-        let hashedPassword = Users.hashPassword(newPassword);
+      if (req.body.Password) {
+        let hashedPassword = Users.hashPassword(req.body.Password);
         updates = {
           Username: newUsername,
           Password: hashedPassword,
@@ -347,7 +297,6 @@ app.put(
         {
           $set: updates,
         },
-        // This line is to specify that the following callback function will take the updated object as parameter
         { new: true }
       )
         .then(function (updatedUser) {
@@ -378,18 +327,18 @@ app.post(
   '/users/:username/topMovies/:movieid',
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const username = req.params.username;
-    const movieId = req.params.movieid;
-    Users.findOne({ Username: username, TopMovies: movieId })
+    Users.findOne({
+      Username: req.params.username,
+      TopMovies: req.params.movieid,
+    })
       .then(function (movieIsPresent) {
         if (movieIsPresent) {
           return res.status(409).send('Movie is already on your list.');
         } else {
           Users.findOneAndUpdate(
-            { Username: username },
+            { Username: req.params.username },
             {
-              // Only adds if not already present (but wouldn't throw error)
-              $addToSet: { TopMovies: movieId },
+              $addToSet: { TopMovies: req.params.movieid },
             },
             { new: true }
           )
@@ -414,12 +363,10 @@ app.delete(
   '/users/:username/topMovies/:movieid',
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const username = req.params.username;
-    const movieId = req.params.movieid;
     Users.findOneAndUpdate(
-      { Username: username },
+      { Username: req.params.username },
       {
-        $pull: { TopMovies: movieId },
+        $pull: { TopMovies: req.params.movieid },
       },
       { new: true }
     )
@@ -433,18 +380,17 @@ app.delete(
   }
 );
 
-// Delete a user by username
+// Deletes a user by username
 app.delete(
   '/users/:username',
   passport.authenticate('jwt', { session: false }),
   function (req, res) {
-    const username = req.params.username;
-    Users.findOneAndRemove({ Username: username })
+    Users.findOneAndRemove({ Username: req.params.username })
       .then(function (user) {
         if (!user) {
-          res.status(400).send(username + ' was not found.');
+          res.status(400).send(req.params.username + ' was not found.');
         } else {
-          res.status(200).send(username + ' was deleted.');
+          res.status(200).send(req.params.username + ' was deleted.');
         }
       })
       .catch(function (err) {
