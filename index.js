c80ff3c0cd0e42229e54b569b6844d94f95472c6
ElenaUJ <elena-ha@web.de>
2023-03-15 10:35:04 +0100

    Configured CORS middleware

--- index.js
+++ index.js
@@ -9,8 +9,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS middleware
 const cors = require('cors');
+let allowedOrigins = [
+  'http://localhost:1234',
+  'https://my-moviemate.netlify.app',
+];
 
-app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          'The CORS policy for this application doesn’t allow access from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 // Authentication (auth.js is handling login endpoint and generating JWT tokens)
 let auth = require('./auth.js')(app);
